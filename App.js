6fb8b0e04af36496ee93ebf5854184b8c06188c4
CSESHROFF <130203287+CSESHROFF@users.noreply.github.com>
2024-02-03 13:53:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+const App = () => {
+
+  let newTime = new Date().toLocaleTimeString();
+
+  const [ctime, setCtime] = useState(newTime);
+
+  const UpdateTime = () =>{
+    let newCTime = new Date().toLocaleTimeString();
+    setCtime(newCTime);
+  }
+
+
+  return (
+    <>
+        <h1>{ctime}</h1>
+        <button onClick={UpdateTime}>Get Time</button>
+    </>
+  );
+}
+
+export default App;
